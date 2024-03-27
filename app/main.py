77a06b6733b8a03f2623bffa1091b7d1c6e7280a
Máté Tallósi <tallosi.mate@simonyi.bme.hu>
2024-03-27 10:09:31 +0100

    Pass stage 2

--- app/main.py
+++ app/main.py
@@ -3,7 +3,15 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
+
+    client_socket, client_address = server_socket.accept()
+    print(f"Connection from {client_address}")
+
+    data = client_socket.recv(1024)
+    print(f"Received {data.decode()}")
+
+    client_socket.sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
+    client_socket.close()
 
 
 if __name__ == "__main__":
