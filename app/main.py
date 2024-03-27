aed0f6e8f69d4db39bcca6411255f06956f59e23
Máté Tallósi <tallosi.mate@simonyi.bme.hu>
2024-03-27 10:23:11 +0100

    Stage 3 pass

--- app/main.py
+++ app/main.py
@@ -1,16 +1,40 @@
 import socket
 
+CRLF = "\r\n"
+
+class HTTPRequest:
+    def __init__(self, data: bytes):
+        self.data = data.decode()
+        self.method, self.path, self.protocol = self.data.split(CRLF)[0].split()
+
+class HTTPResponse:
+    def __init__(self, status_code: int):
+        self.status_code = status_code
+
+    def __str__(self):
+        STATUS_CODE_STRINGS = {
+            200: "OK",
+            404: "Not Found",
+        }
+
+        return f"HTTP/1.1 {self.status_code} {STATUS_CODE_STRINGS[self.status_code]}{CRLF}{CRLF}"
+
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
+    # Start listening for incoming connections
     client_socket, client_address = server_socket.accept()
-    print(f"Connection from {client_address}")
 
+    # Receive data from the client and parse the HTTP request
     data = client_socket.recv(1024)
-    print(f"Received {data.decode()}")
+    request = HTTPRequest(data)
 
-    client_socket.sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
+    # Make a decision based on the path of the request
+    response = HTTPResponse(200) if request.path == "/" else HTTPResponse(404)
+
+    # Send the response back to the client
+    client_socket.sendall(response.__str__().encode())
     client_socket.close()
 
 
